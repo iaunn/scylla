382d239319cd72c1f535b6633217dda51e523bb1
Siriwat Wanghorm <siriwat_aun106@hotmail.com>
2020-06-04 10:38:03 +0700

    update pubproxy_provider

--- scylla/providers/pubproxy_provider.py
+++ scylla/providers/pubproxy_provider.py
@@ -5,4 +5,11 @@ class PubproxyProvider(PlainTextProvider):
     def urls(self) -> [str]:
         return [
             'http://pubproxy.com/api/proxy?limit=5&format=txt&type=http&level=anonymous&last_check=60&no_country=CN',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=http&level=anonymous&last_check=60&no_country=TH',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=http&level=anonymous&last_check=60&no_country=SG',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=http&level=anonymous&last_check=60&no_country=HK',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=https&level=anonymous&last_check=60&no_country=CN',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=https&level=anonymous&last_check=60&no_country=TH',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=https&level=anonymous&last_check=60&no_country=SG',
+            'http://pubproxy.com/api/proxy?limit=5&format=txt&type=https&level=anonymous&last_check=60&no_country=HK',
         ]
